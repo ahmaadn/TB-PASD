0d62378b35f7c500e6701a7d5ebc22c56a4e0c5e
Ahmad nur <ahmadnursahid12345@gmail.com>
2023-06-01 11:08:11 +0700

    rewite userman

--- src/UserMan/userman.cpp
+++ src/UserMan/userman.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 #define DB_NAME "./src/users.csv"
 
@@ -15,72 +16,130 @@ struct UserMan {
 
 // global variable untuk menampung data users
 fstream DB_USERS;
+vector<UserMan> DB;
+UserMan *curr_user = nullptr;
 
+
+void init();
 void login();
-void signin();
-UserMan find_user(string username, string password);
+bool find_user(string username, string password);
+void update_user(UserMan &user);
+void store_user();
+void display();
 
-int main(){
+int main() {
+    init();
     login();
+
+    // TODO: DEBUG
+    cout << "Curr User: " << endl;
+    cout << curr_user->username << ":" << curr_user->password << ":" << curr_user->role << endl << endl;
+
+    update_user(*curr_user);
+    display(); // Display semua user
+    store_user();
+
+    // Cek apakah ada perubahan di dalam file csv
+    cout << "---------" << endl;
+    cout << "Get all" << endl;
+
+    init();    // Memuat ulang file
+    display(); // apakah user
 }
 
-void login(){
-    string username, password;
-    UserMan user;
-    while (true){
-        cout << "Masukkan Username : "; cin >> username;
-        cout << "Masukkan password : "; cin >> password;
+void init() {
+    DB.clear();
 
-        user = find_user(username, password);
-
-        if (user.role == "admin") {
-            // admin
-            cout << "Selamat datang admin";
-            break;
-        } else if (user.role == "stack"){
-            // stack user
-            cout << "Selamat datang user Stack";
-            break;
-        } else if (user.role == "queue") {
-            // Queue User
-            cout << "Selamat datang user queue";
-            break;
-        }
-        // block user yang tidak dapat akses
-        cout << "Tidak dapat ijin akses" << endl;
-        continue;
-    }
-}
-
-void signin(){
-
-}
-
-
-UserMan find_user(string username, string password){
     DB_USERS.open(DB_NAME);
     string line;
-    int pos;
 
-    // skip satu baris
+    // skip 1 baris
     getline(DB_USERS, line);
 
-    while (getline(DB_USERS, line)){
+    while (getline(DB_USERS, line)) {
         stringstream ss(line);
-        UserMan user_temp;
+        UserMan user_tmp;
 
-        getline(ss, user_temp.username, ',');
-        getline(ss, user_temp.password, ',');
-        getline(ss, user_temp.role);
-        if (user_temp.username == username && user_temp.password == password){
-            DB_USERS.close();
-            return user_temp;
+        getline(ss, user_tmp.username, ',');
+        getline(ss, user_tmp.password, ',');
+        getline(ss, user_tmp.role);
+
+        // Menyimpan semua user ke vector
+        DB.push_back(user_tmp);
+    }
+    DB_USERS.close();
+}
+
+void login() {
+    string username, password;
+
+    while (true) {
+        bool auth = false;
+
+        cout << "Masukkan Username : ";
+        cin >> username;
+        cout << "Masukkan password : ";
+        cin >> password;
+
+        auth = find_user(username, password);
+        if (!auth) {
+            cout << "Tidak dapat ijin akses" << endl;
+            continue;
+        }
+
+        if (curr_user->role == "admin") {
+            // admin
+            cout << "Selamat datang admin (" << curr_user->username << ")" << endl;
+            break;
+        } else if (curr_user->role == "stack") {
+            // stack user
+            cout << "Selamat datang user Stack (" << curr_user->username << ")" << endl;
+            break;
+        } else if (curr_user->role == "queue") {
+            // Queue User
+            cout << "Selamat datang user queue (" << curr_user->username << ")" << endl;
+            break;
         }
     }
+}
 
-    // return none user
-    // login tidak valid
-    DB_USERS.close();
-    UserMan none_user;
-    return none_user;
-};
+bool find_user(string username, string password) {
+    for (UserMan& user : DB) {
+        if (user.username == username && user.password == password) {
+            curr_user = &user;
+            return true;
+        }
+    }
+    return false;
+}
+
+void update_user(UserMan& user) {
+    // TODO: WIP
+    cout << "Username Baru  : ";
+    cin >> user.username;
+    cout << "Password Baru  : ";
+    cin >> user.password;
+    cout << "Role Baru      : ";
+    cin >> user.role;
+}
+
+void store_user() {
+    DB_USERS.open(DB_NAME, ios::out | ios::trunc);
+    DB_USERS << "username,password,role" << endl;
+
+    for (UserMan& user : DB) {
+        DB_USERS << user.username << ",";
+        DB_USERS << user.password << ",";
+        DB_USERS << user.role << endl;
+    }
+    DB_USERS.close(); // Menutup file setelah selesai
+}
+
+void display() {
+    // TODO: WIP
+    cout << "----------" << endl;
+    cout << "USERS (" << DB.size() << ")" << endl;
+    for (UserMan& user : DB) {
+        cout << user.username << ":" << user.password << ":" << user.role << endl;
+    }
+}
